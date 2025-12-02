f564d6eda4ac7000d5559f0fa0307cc1ee066911
Danny Piper <danny.piper@motorolasolutions.com>
2025-12-02 15:34:55 +0000

    feat: added part 2

--- day02/main.go
+++ day02/main.go
@@ -4,9 +4,10 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"time"
 )
 
-func isInvalid(id int64) bool {
+func isInvalidPart1(id int64) bool {
 	idStr := fmt.Sprintf("%d", id)
 	if len(idStr)%2 == 1 {
 		return false
@@ -18,11 +19,69 @@ func isInvalid(id int64) bool {
 	return lowerStr == upperStr
 }
 
-func addInvalidIds(ids [2]int64) int64 {
+func addInvalidIdsPart1(ids [2]int64) int64 {
 	var ret int64
 	for id := ids[0]; id <= ids[1]; id++ {
-		if isInvalid(id) {
+		if isInvalidPart1(id) {
 			log.Printf("Invalid ID: %d", id)
+			ret += id
+		}
+	}
+
+	return ret
+}
+
+func isInvalidPart2(id int64) bool {
+	idStr := fmt.Sprintf("%d", id)
+
+	// Special case factor of 1
+	eq := true
+	for i := 1; i < len(idStr); i++ {
+		if idStr[i] != idStr[0] {
+			eq = false
+			break
+		}
+	}
+
+	if eq {
+		log.Printf("Invalid Id Factor: 1, Id: %d", id)
+		return true
+	}
+
+	// Slower checks for larger symmetries
+	for factor := 2; factor <= len(idStr)/2; factor++ {
+		if len(idStr)%factor != 0 {
+			continue
+		}
+
+		parts := make([]string, factor)
+		partLen := len(idStr) / factor
+
+		for i := range factor {
+			parts[i] = idStr[i*int(partLen) : (i+1)*partLen]
+		}
+
+		eq := true
+		for i := 1; i < len(parts); i++ {
+			if parts[i] != parts[0] {
+				eq = false
+				break
+			}
+		}
+
+		if eq {
+			log.Printf("Invalid Id Factor: %d, Part: %s, Id: %d", factor, parts[0], id)
+			return true
+		}
+	}
+
+	return false
+}
+
+func addInvalidIdsPart2(ids [2]int64) int64 {
+	var ret int64
+	for id := ids[0]; id <= ids[1]; id++ {
+		if isInvalidPart2(id) {
 			ret += id
 		}
 	}
@@ -36,8 +95,10 @@ func main() {
 		log.Fatalf("Cannot read file: %s", err)
 	}
 
+	t := time.Now()
 	var num int64
 	var part1Acc int64
+	var part2Acc int64
 	nums := [2]int64{}
 	for i, c := range bytes {
 		switch {
@@ -52,12 +113,15 @@ func main() {
 		case c == ',':
 			nums[1] = num
 			num = 0
-			part1Acc += addInvalidIds(nums)
+			part1Acc += addInvalidIdsPart1(nums)
+			part2Acc += addInvalidIdsPart2(nums)
 
 			// Reset the state
 			nums = [2]int64{}
 		}
 	}
 
+	log.Printf("Time: %s", time.Since(t))
 	log.Printf("Part one answer: %d", part1Acc)
+	log.Printf("Part two answer: %d", part2Acc)
 }
