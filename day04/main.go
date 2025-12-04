bdc953545f8b655fe0c64e1094f927512cb150b4
Danny Piper <danny.piper@motorolasolutions.com>
2025-12-04 11:16:14 +0000

    feat: added faster solution

--- day04/main.go
+++ day04/main.go
@@ -7,7 +7,8 @@ import (
 )
 
 type Grid struct {
-	Grid [][]bool
+	Grid      [][]bool
+	CountGrid [][]int
 }
 
 func (g *Grid) CountNeighbours(xPos, yPos int) int {
@@ -67,6 +68,56 @@ func (g *Grid) Part2() int {
 	return count
 }
 
+func (g *Grid) FastPart2() int {
+	g.CountGrid = make([][]int, len(g.Grid))
+
+	for y := 0; y < len(g.Grid); y++ {
+		g.CountGrid[y] = make([]int, len(g.Grid[y]))
+
+		for x := 0; x < len(g.Grid[y]); x++ {
+			if g.Grid[y][x] {
+				g.CountGrid[y][x] = g.CountNeighbours(x, y)
+			}
+		}
+	}
+
+	count := 0
+	dirty := true
+
+	for dirty {
+		dirty = false
+
+		for yPos := 0; yPos < len(g.CountGrid); yPos++ {
+			for xPos := 0; xPos < len(g.CountGrid[yPos]); xPos++ {
+				if !g.Grid[yPos][xPos] {
+					continue
+				}
+
+				if g.CountGrid[yPos][xPos] < 4 {
+					g.Grid[yPos][xPos] = false
+					count++
+					dirty = true
+
+					// All of the neighbours have one less now
+					for y := yPos - 1; y <= yPos+1; y++ {
+						for x := xPos - 1; x <= xPos+1; x++ {
+							if y < 0 || x < 0 {
+								continue
+							} else if y >= len(g.Grid) || x >= len(g.Grid[y]) {
+								continue
+							}
+
+							g.CountGrid[y][x]--
+						}
+					}
+				}
+			}
+		}
+	}
+
+	return count
+}
+
 func main() {
 	bytes, err := os.ReadFile("./input.txt")
 	if err != nil {
@@ -92,6 +143,7 @@ func main() {
 	}
 
 	log.Printf("Part one answer: %d", grid.Part1())
-	log.Printf("Part two answer: %d", grid.Part2())
+	// log.Printf("Part two answer: %d", grid.Part2())
+	log.Printf("Part two (fast) answer: %d", grid.FastPart2())
 	log.Printf("Time taken: %s", time.Since(t))
 }
